ae5d88801d5c9c8faf61cd17ad7388d7dc14335f
Muhammad <boukerfa.ma@gmail.com>
2024-12-13 01:12:44 +0100

    Add mappings for LOV attributes

--- config/mapping.rb
+++ config/mapping.rb
@@ -3,16 +3,24 @@ DATA_MAPPING = {
     "acronym" => {
       "lov_class" => "Vocabulary",
       "lov_type" => "vann:preferredNamespacePrefix",
-      "optional" => false
+      "optional" => false,
+      "pk" =>true   
     },
     "name" => {
       "lov_class" => "Vocabulary",
       "lov_type" => "dcterms:title",
       "lang" => "en",
       "optional" => false
-    }
+    },
   },
+  
   "Submission" => {
+    "acronym" => {
+      "lov_class" => "Vocabulary",
+      "lov_type" => "vann:preferredNamespacePrefix",
+      "optional" => false,
+      "pk" => true,
+    },
     "description" => {
       "lov_class" => "Vocabulary",
       "lov_type" => "dcterms:description",
@@ -22,8 +30,57 @@ DATA_MAPPING = {
     "URI" => {
       "lov_class" => "catalog",
       "lov_type" => "foaf:primaryTopic",
-      "optional" => false
-    }
+      "optional" => false,
+    },
+    "released" => {
+      "lov_class" => "Distribution",
+      "lov_type" => "dcterms:issued",
+      "optional" => false,
+      "pk" =>true
+    },
+    "pullLocationn" => {
+      "lov_class" => "Vocabulary",
+      "lov_type" => "<http://www.w3.org/ns/dcat#distribution>",
+      "optional" => false,
+      "pk" => true
+    },
+    "homepage" => {
+      "lov_class" => "Vocabulary",
+      "lov_type" => "foaf:homepage",
+      "optional" => true,
+    },
+    "metadataVoc" => {
+      "lov_class" => "Distribution",
+      "lov_type" => "voaf:metadataVoc",
+      "optional" => true,
+      "isArray" => true
+    },
+    "keywords" => {
+      "lov_class" => "Vocabulary",
+      "lov_type" => "dcat:keyword",
+      "optional" => true,
+      "isArray" => true
+    },
+    # Agents 
+    "hasContributor" => {
+      "lov_class" => "Vocabulary",
+      "lov_type" => "dcat:keyword",
+      "optional" => true,
+      "isArray" => true
+    },
+    # Relations
+    "generalizes" => {
+      "lov_class" => "Distribution",
+      "lov_type" => "voaf:generalizes",
+      "optional" => true,
+      "isArray" => true
+    },
+    "explanationEvolution" => {
+      "lov_class" => "Distribution",
+      "lov_type" => "voaf:specializes",
+      "optional" => true,
+      "isArray" => true
+    },
   },
   
   "default" => {
@@ -32,10 +89,12 @@ DATA_MAPPING = {
     },
     "Submission" => {
       "hasOntologyLanguage" => "OWL",
-      "status" => "production"
+      "status" => "production",
+      "contact" => ["name":"admin","email":"admin@admin.com"],
+      "includedInDataCatalog" => ["https://lov.linkeddata.es/dataset/lov"],
     },
     "Agent" => {
       "creator" => "admin"
     }
-  }
+  },
 }
