5a85c32f40b5166eee9e5309b61ce362d0cb259b
Muhammad <boukerfa.ma@gmail.com>
2024-12-13 15:01:16 +0100

    handle extracting agents Ids based on their names

--- entities/submission.rb
+++ entities/submission.rb
@@ -26,6 +26,26 @@ class Submission < BaseEntity
 
     @uploadFilePath = UploadIO.new(File.open(file_path), 'text/n3', File.basename(file_path))
   end  
+def load_default_values(data)
+    if data
+      data.each do |key, property|
+      instance_variable_set("@#{key}", property)
+      end
+    end
+    instance_variable_set("@ontology", @acronym.upcase)
+  end
+  def get_agents_id
+    AGENTS_ATTRIBUTES.each do |attribute|
+      next unless instance_variable_defined?("@#{attribute}") && instance_variable_get("@#{attribute}").is_a?(Array)
 
-  
+      # Get the current value of the instance variable and update it
+      updated_agents = instance_variable_get("@#{attribute}").map do |agent_name|
+        # Call the method to get the agent ID
+        Agent.get_agent_id_by_name(agent_name)
+      end
+
+      # Set the updated value back to the instance variable
+      instance_variable_set("@#{attribute}", updated_agents)
+    end
+  end  
 end
