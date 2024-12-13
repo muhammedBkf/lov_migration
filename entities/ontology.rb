6ec7d943b44464d3d9bede34c7aaf6b71d846abb
Muhammad <boukerfa.ma@gmail.com>
2024-12-13 14:59:36 +0100

    add a function to fetch all ontologies in a OntoPortal instance

--- entities/ontology.rb
+++ entities/ontology.rb
@@ -6,4 +6,10 @@ class Ontology < BaseEntity
   def self.upload_endpoint
     "ontologies"
   end
+
+  def self.fetch_portal_all(portalUrl)
+    url = URI("#{portalUrl}/ontologies")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  end
 end
