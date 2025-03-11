558fb8f3e274297abfb650e72fa4da3e0bc8ac83
AmineBKF <boukerfa.ma@gmail.com>
2025-03-11 10:32:13 +0100

    check for new submissions and upload them

--- entities/submission.rb
+++ entities/submission.rb
@@ -44,6 +44,72 @@ end
 
     @uploadFilePath = UploadIO.new(File.open(file_path), 'text/n3', File.basename(file_path))
   end  
+
+  def reset_agents
+    # Parse the LOVPORTAL_ENDPOINT URL
+    url = URI.parse(LOVPORTAL_ENDPOINT)
+    
+    # Construct the PATCH request to reset agents
+    request = Net::HTTP::Patch.new("#{url}/ontologies/#{@acronym.upcase}/latest_submission")
+    request['Authorization'] = "apikey token=#{ENV['API_KEY']}"
+    request['content-type'] = 'multipart/form-data'
+    
+    # Define the form data to reset agents
+    form_data = [
+      ["hasContributor", "[]"],
+      ["hasCreator", "[]"],
+      ["hasPublisher", "[]"],
+      ["curatedBy", "[]"]
+    ]
+    request.set_form(form_data, 'multipart/form-data')
+  
+    # Execute the request
+    response = Net::HTTP.start(url.hostname, url.port) do |http|
+      http.request(request)
+    end
+  
+    # Return the response
+    response
+  end
+  # This is used to upload an entity (Agent, Ontology, Submission)
+  def update
+    url = URI.parse(LOVPORTAL_ENDPOINT)
+    
+    reset_agents
+
+    request = Net::HTTP::Patch.new("#{url}/ontologies/#{@acronym.upcase}/latest_submission")
+    request['Authorization'] = "Authorization: apikey token=#{ENV['API_KEY']}"
+    request['content-type'] = 'multipart/form-data'
+  
+    # Set form data
+    form_data = to_multipart_form_data
+    request.set_form form_data, 'multipart/form-data'
+
+    # Execute the request
+    response = Net::HTTP.start(url.hostname, url.port) do |http|
+      http.request(request)
+    end
+  end
+
+    # Helper function to fetch the latest submission version from LOVPORTAL_ENDPOINT
+    def fetch_latest_submission_version
+      # Construct the URL for the latest submission
+
+      url = URI.parse("#{LOVPORTAL_ENDPOINT}/ontologies/#{@acronym.upcase}/latest_submission")
+      
+      # Send a GET request to the endpoint
+      response = Net::HTTP.get_response(url)
+
+      if response.is_a?(Net::HTTPSuccess)
+        # Parse the response body to extract the version
+        latest_submission = JSON.parse(response.body)
+        latest_submission_version = latest_submission['version']
+        return latest_submission_version
+      else
+        raise "Failed to fetch latest submission version for #{acronym}. Response code: #{response.code}"
+      end
+    end
+
 def load_default_values(data)
     if data
       data.each do |key, property|
